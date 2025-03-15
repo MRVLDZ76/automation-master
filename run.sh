2f3ebf3bf02554dc32c559120f76781668545908
MRVLDZ <iancasillasbuffon@gmail.com>
2025-03-15 10:31:33 +0100

    bash files

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-
-gunicorn automation.wsgi:application  --log-file -
+# Restart Gunicorn
+gunicorn --workers=4 --threads=2 --worker-class=gthread --worker-tmp-dir=/dev/shm --timeout=60 --keep-alive=5 --max-requests=1000 --max-requests-jitter=100 --backlog=2048 --bind=0.0.0.0:8000 --access-logfile=- --error-logfile=- automation.wsgi:application
 
 echo "Application is running."
